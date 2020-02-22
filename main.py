29246c7b81117a75b931f7893ea2b3eaa714a81a
Joy Prakash Sharma <droidlabour@gmail.com>
2020-02-22 16:45:48 +0530

    Fix ZipFile length cannot exceed 4096 characters

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import boto3
 
 athena = boto3.client('athena')
 s3 = boto3.client('s3')
-db_name = os.getenv('AthenaDbName')
-output_bucket = os.getenv('OutputBucket')
-create_table_tpl = """
+dbName = os.getenv('AthenaDbName')
+oputBucket = os.getenv('OutputBucket')
+createTable = """
 CREATE EXTERNAL TABLE IF NOT EXISTS
   {}.{} {}
   ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
@@ -21,11 +21,11 @@ CREATE EXTERNAL TABLE IF NOT EXISTS
 """
 
 
-def query_status(qid):
+def queryStatus(qid):
     return athena.get_query_execution(QueryExecutionId=qid)['QueryExecution']['Status']['State']
 
 
-def create_columns(columns):
+def createColumns(columns):
     x = []
     for i in columns:
         i = i.strip().lower()
@@ -35,8 +35,8 @@ def create_columns(columns):
     return x
 
 
-def wait_query_to_finish(qid):
-    while query_status(qid) in ['QUEUED', 'RUNNING']:
+def wait4Query(qid):
+    while queryStatus(qid) in ['QUEUED', 'RUNNING']:
         print("Waiting query id to finish {}".format(qid))
         sleep(5)
     print("query id finished {}".format(qid))
@@ -46,8 +46,8 @@ def run_query(query):
     print("Running query: {}".format(query))
     qid = athena.start_query_execution(
         QueryString=query,
-        QueryExecutionContext={'Database': db_name},
-        ResultConfiguration={'OutputLocation': output_bucket}
+        QueryExecutionContext={'Database': dbName},
+        ResultConfiguration={'OutputLocation': oputBucket}
     )['QueryExecutionId']
     print("query id: {}".format(qid))
     return qid
@@ -57,9 +57,9 @@ def handler(event, context):
     print(json.dumps(event))
 
     try:
-        query = "CREATE DATABASE IF NOT EXISTS {};".format(db_name)
+        query = "CREATE DATABASE IF NOT EXISTS {};".format(dbName)
         query_id = run_query(query)
-        wait_query_to_finish(query_id)
+        wait4Query(query_id)
 
         for r in event['Records']:
             bucket = r['s3']['bucket']['name']
@@ -91,15 +91,15 @@ def handler(event, context):
                 b = _.split(',')
                 if len(a) > len(b):
                     serde_prop = "'separatorChar' = '|', 'serialization.format' = ',', 'field.delim' = '|'"
-                    columns = create_columns(a)
+                    columns = createColumns(a)
                 else:
                     serde_prop = "'serialization.format' = ',', 'field.delim' = ','"
-                    columns = create_columns(b)
+                    columns = createColumns(b)
             columns = '(' + ', ' . join(columns) + ')'
 
-            query = create_table_tpl.format(db_name, table_name, columns, serde_prop, location)
+            query = createTable.format(dbName, table_name, columns, serde_prop, location)
             query_id = run_query(query)
-            wait_query_to_finish(query_id)
+            wait4Query(query_id)
 
             x = csv_path.split('/')[:-1]
             x.append('views/')
@@ -115,7 +115,7 @@ def handler(event, context):
                         view_name = table_name + '_' + str(k) + '_view'
                         query = v.read().format(view_name, table_name)
                         query_id = run_query(query)
-                        wait_query_to_finish(query_id)
+                        wait4Query(query_id)
     except Exception as e:
         print(str(e))
         traceback.print_exc()
